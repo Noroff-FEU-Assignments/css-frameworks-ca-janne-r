f68ec76bc59ca9ac440bd207609ed25391131947
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-11-19 10:41:43 +0100

    border radius accordion

--- src/components/ShareIcons.js
+++ src/components/ShareIcons.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 function ShareIcons() {
   return (
 
-    <Row className="shareIcons">
-      <Col xs={12} sm={true} >
-        <p>SHARE</p>
+
+
+    <div className="shareIcons">
+      <p>SHARE</p>
+      <div>
         <i className="fab fa-facebook-f"></i>
         <i className="fab fa-twitter"></i>
-      </Col>
-    </Row>
+      </div>
+    </div>
+
+
   );
 
 }
